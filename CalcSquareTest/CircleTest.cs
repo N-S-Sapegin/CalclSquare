599d72ef39da7850787e36ce6434edbf0204ea0a
n.s.sapegin <n.s.sapegin@mail.ru>
2022-03-24 17:31:38 +0300

    Create test files

---
+++ CalcSquareTest/CircleTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using CalculateSquare;
+
+namespace CalcSquareTest
+{
+    public class CircleTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ CalcSquareTest/TriangleTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using CalculateSquare;
+
+namespace CalcSquareTest
+{
+    public class TriangleTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
