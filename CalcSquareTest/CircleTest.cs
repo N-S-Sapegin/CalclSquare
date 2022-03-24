c5b73bd5ae29858cb46fd7c1298b28e428a88d9b
n.s.sapegin <n.s.sapegin@mail.ru>
2022-03-24 18:09:39 +0300

    write test

--- CalcSquareTest/CircleTest.cs
+++ CalcSquareTest/CircleTest.cs
@@ -5,15 +5,38 @@ namespace CalcSquareTest
 {
     public class CircleTest
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void CircleSquareTest()
         {
-            Assert.Pass();
+            // Arrange
+            double result;
+            Circle circle = new Circle(3);
+            // Act
+            result = circle.Square();
+            // Assert
+            Assert.AreEqual(28.274333882308138, result);
+        }
+        [Test]
+        public void CircleABSTest()
+        {
+            // Arrange
+            double result;
+            Circle circle = new Circle(-3);
+            // Act
+            result = circle.Square();
+            // Assert
+            Assert.AreEqual(28.274333882308138, result);
+        }
+        [Test]
+        public void CircleZeroSquareTest()
+        {
+            // Arrange
+            double result;
+            Circle circle = new Circle(0);
+            // Act
+            result = circle.Square();
+            // Assert
+            Assert.AreEqual(0, result);
         }
     }
 }
--- CalcSquareTest/TriangleTest.cs
+++ CalcSquareTest/TriangleTest.cs
@@ -1,19 +1,67 @@
 ﻿using NUnit.Framework;
 using CalculateSquare;
+using System;
 
 namespace CalcSquareTest
 {
     public class TriangleTest
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void TriangleSquareTest()
         {
-            Assert.Pass();
+            // Arrange
+            double result;
+            Triangle triangle = new Triangle(2, 1, 2);
+            // Act
+            result = triangle.Square();
+            // Assert
+            Assert.AreEqual(0.9682458365518543, result);
+        }
+        [Test]
+        public void TriangleABSTest()
+        {
+            // Arrange
+            double result;
+            Triangle triangle = new Triangle(-3, -4, -5);
+            // Act
+            result = triangle.Square();
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+        [Test]
+        public void NotTriangleTest()
+        {
+            try
+            {
+                new Triangle(0, 4, 5);
+                Assert.Fail("Exception");
+            }
+            catch (ArgumentException)
+            {
+
+            }
+        }
+        [Test]
+        public void IsRectangularCorrectTest()
+        {
+            // Arrange
+            bool result;
+            Triangle triangle = new Triangle(3, 4, 5);
+            // Act
+            result = triangle.IsRectangular();
+            // Assert
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void IsRectangularWrongTest()
+        {
+            // Arrange
+            bool result;
+            Triangle triangle = new Triangle(2, 4, 5);
+            // Act
+            result = triangle.IsRectangular();
+            // Assert
+            Assert.IsFalse(result);
         }
     }
 }
