f8386fc990d8e991a95347e57705e06e5dceeccb
n.s.sapegin <n.s.sapegin@mail.ru>
2022-03-24 17:23:07 +0300

    Create main classes

---
+++ CalculateSquare/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateSquare
+{
+    public class Circle : Figure
+    {
+        public override double Square()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
